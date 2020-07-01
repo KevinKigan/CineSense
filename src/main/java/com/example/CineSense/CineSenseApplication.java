c9b3f371869440896ff63bf1f7b2f5cd76a93c31
Kevin Gómez <kevingomez.a98@gmail.com>
2020-07-01 16:36:43 +0200

    first commit

---
+++ src/main/java/com/example/CineSense/CineSenseApplication.java
@@ -0,0 +1,13 @@
+package com.example.CineSense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineSenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineSenseApplication.class, args);
+	}
+
+}
